3f60d94bdf9b17846caef4878d8c0866e1d6ba57
ekelemene <ksuxaskrip2007@gmail.com>
2025-04-17 10:22:10 +0300

    Add files via upload

---
+++ prac37/IInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac37
+{
+    interface IInterface
+    {
+        void Method();
+
+
+
+
+    }
+}
---
+++ prac37/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dClass = new DerivedClass();
+            dClass.Method();
+            Console.Read();
+        }
+    }
+}
